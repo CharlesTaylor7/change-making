1d5c6522c59f4de2c9467f92234b2299cca377f0
Charles Taylor <charlestaylor95@gmail.com>
2019-10-26 13:20:06 -0400

    Make test suite readable

--- test/ChangeMakingSpec.hs
+++ test/ChangeMakingSpec.hs
@@ -1,39 +1,31 @@
 module ChangeMakingSpec where
 
-import Data.Function ((&))
-
-import Data.Map (Map)
-import qualified Data.Map as M
-
 import Test.Hspec
 
 import ChangeMaking
 
 spec = do
   describe "ChangeMaking" $ do
-    describe "makeChangeWith" $ do
-      it "works" $ do
-        sequenceA testCases
-        pure ()
+    describe "makeChange: uses standard usa coins" $ do
+      it "makes change for a penny" $ do
+        makeChange (Money 1) `shouldBe` [penny]
+      it "makes change for a quarter" $ do
+        makeChange (Money 25) `shouldBe` [quarter]
+      it "makes change for 40 cents" $ do
+        makeChange (Money 40) `shouldBe` [quarter, dime, nickel]
+      it "makes change for 20 cents" $ do
+        makeChange (Money 20) `shouldBe` [dime, dime]
+      it "makes change for 100 cents" $ do
+        makeChange (Money 100) `shouldBe` [quarter, quarter, quarter, quarter]
+      it "makes change for 73 cents" $ do
+        makeChange (Money 73) `shouldBe` [quarter, quarter, dime, dime, penny, penny, penny]
 
-testCases :: [IO ()]
-testCases =
-  [
-    makeChange (Money 1) `shouldBe` [penny],
-    makeChange (Money 25) `shouldBe` [quarter],
-    makeChange (Money 40) `shouldBe` [quarter, dime, nickel],
-    makeChange (Money 20) `shouldBe` [dime, dime],
-    makeChange (Money 100) `shouldBe` [quarter, quarter, quarter, quarter],
-    makeChange (Money 73) `shouldBe` [quarter, quarter, dime, dime, penny, penny, penny],
-    let
-      coinSet = Currency $ Coin <$> [50, 20, 15, 5, 4, 1]
-      expected = Coin <$> [15, 4, 4]
-    in
-      makeChangeWith coinSet (Money 23) `shouldBe` expected,
-    let
-      coinSet = Currency $ Coin <$> [1, 5, 10, 21, 25]
-      expected = Coin <$> [21, 21, 21]
-    in
-      makeChangeWith coinSet (Money 63) `shouldBe` expected,
-      pure ()
-  ]
+    describe "makeChangeWith: allows custome currency" $ do
+      it "makes change for 23 cents" $ do
+        let coinSet = Currency $ Coin <$> [50, 20, 15, 5, 4, 1]
+        let expected = Coin <$> [15, 4, 4]
+        makeChangeWith coinSet (Money 23) `shouldBe` expected
+      it "makes change for 63 cents" $ do
+        let coinSet = Currency $ Coin <$> [1, 5, 10, 21, 25]
+        let expected = Coin <$> [21, 21, 21]
+        makeChangeWith coinSet (Money 63) `shouldBe` expected
